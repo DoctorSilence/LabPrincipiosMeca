28f60c854b151916e8a63ef97ef2d3d5d5927eb2
Victor <Daniel@Air-de-Daniel.lan>
2018-02-11 23:18:11 -0600

    Se añadió la función para implementar un suscriptor

--- P2/pract2ArduinoRos/conexRos.ino
+++ P2/pract2ArduinoRos/conexRos.ino
@@ -4,13 +4,25 @@ ros::NodeHandle nh;
 std msgs :: String str msg;
 ros :: Publisher pub(”arduino msg”, &str msg );
 
+// Declarar suscriptor
+ros :: Subscriber<std msgs :: String> sub(”other msg”, &message ros );
+
 void setup() {
   nh.initNode();
   nh.advertise(pub);
+
+  // Inicializar suscriptor
+  nh.subscribe(sub);
 }
 
 void loop() {
+  // Para publicar un mensaje
   str msg.data = string variable;
   chatter.publish(&str_msg);
   nh.spinOnce();
+}
+
+// Implementar un suscriptor
+void message_ros(const std_msgs::String& ros_msg){
+  string_variable = ros_msg.data;
 }
