47a3629681e78f902b508c77d9465724b04864b1
Víctor Cruz <vcruzgon@itam.mx>
2018-02-15 01:00:41 -0600

    Se escribió código tratando de establecer una comunicación entre ROS y Arduino

---
+++ P2/pract2ArduinoRos/conexRos.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+
+#include <iostream>
+#include <std_msgs/Int32.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	ros::init(argc,argv,"ejemplo_pub_node");
+	ros::NodeHandle nh;
+	ROS_INFO_STREAM("ejemplo_pub_node initialized");
+
+	ros::Publisher pub = nh.advertise<std_msgs::Int32> ("/msgArduino", 1);
+	ros::Subscriber sub_vel = nh.subscribe("/msgFromArduino", 1000, &get_msg);
+
+	std_msgs::Int32 msg;
+	int num;
+
+	while (ros::ok())
+	{
+
+    cout << "Introduce un numero entero:" << endl;
+		cin >> num;
+
+		msg.data = num;
+		pub.publish(msg);
+
+    ROS_INFO_STREAM(ros::this_node::getName());
+	}
+
+    return 0;
+}
--- P2/pract2ArduinoRos/conexRos.ino
+++ P2/pract2ArduinoRos/conexRos.ino
@@ -2,10 +2,14 @@
 
 ros::NodeHandle nh;
 std msgs :: String str msg;
-ros :: Publisher pub(”arduino msg”, &str msg );
+ros :: Publisher pub("/msgFromArduino", &str msg );
 
 // Declarar suscriptor
-ros :: Subscriber<std msgs :: String> sub(”other msg”, &message ros );
+ros :: Subscriber<std msgs :: String> sub("/msgArduino", &message ros );
+
+// LED
+int pinButton = 8;
+int LED = 2;
 
 void setup() {
   nh.initNode();
@@ -13,16 +17,33 @@ void setup() {
 
   // Inicializar suscriptor
   nh.subscribe(sub);
+
+  // LED
+  pinMode(pinButton, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // Para publicar un mensaje
+  ledStatus(1);
   str msg.data = string variable;
   chatter.publish(&str_msg);
   nh.spinOnce();
+  ledStatus(0)
 }
 
 // Implementar un suscriptor
 void message_ros(const std_msgs::String& ros_msg){
   string_variable = ros_msg.data;
+}
+
+// 0 - Apagado
+// 1 - Prendido
+void ledStatus(int status){
+  if (status) {
+    digitalWrite(LED, HIGH);
+  }
+  else{
+    digitalWrite(LED, LOW);
+  }
 }
